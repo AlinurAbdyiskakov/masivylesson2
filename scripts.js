68bfcdbfa7015fe6c5230164741393aa2a4a0312
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-02 14:50:36 +0600

    add

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let dw = text => document.write(text + "<br>")
+// // let example1 = (a, b) => a % b;
+// // console.log(example1(5 % 2));
+// for (let number1 = 2;number1 <=10; number1++)
+// if (number1 +++ 2 == 0);
+// // document.write(number1)log
+
+// var step;
+// for (step = 0; step < 5; step++) {
+//   // Запускается 5 раз, с шагом от 0 до 4.
+//   console.log('Идём 1 шаг на восток');
+// }
+let example1 = [4, 3, -5, 6, 0];
+dw(example1[3]);
+let example2 = [0, 0, 5, -1];
+dw(example2[2]);
+let example3 = [0,-1,-5,-50];
+dw(example3[0]);
